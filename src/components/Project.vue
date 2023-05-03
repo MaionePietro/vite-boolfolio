fcabce369d59737499e70d6f1a028df8dfffbdec
MaionePietro <pietromaionespam@gmail.com>
2023-05-03 23:41:36 +0200

    FEAT card with component

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,6 +1,10 @@
 <script>
+import ProjectCard from './ProjectCard.vue'
 import axios from 'axios'
 export default {
+    components:{
+        ProjectCard
+    },
     data() {
         return {
             projects: []
@@ -27,16 +31,7 @@ export default {
 
 <template lang="">
     <div class="row row-cols-4">     
-        <div class="col" v-for="(project, i) in projects" :key="project.id">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">{{ project.title }}</h5>
-                    <p class="card-text">{{ project.costumer }}</p>
-                    <p class="card-text">{{ project.description }}</p>
-                    <a href="">{{ project.url }}</a>
-                </div>
-            </div>
-        </div>
+        <ProjectCard v-for="(project, i) in projects" :key="project.id" :project="project" />
     </div>
 </template>
 
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: {
+        project: {
+            type: Object,
+            required: true
+        }
+    },
+}
+</script>
+<template lang="">
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ project.title }}</h5>
+                    <p class="card-text">{{ project.costumer }}</p>
+                    <p class="card-text">{{ project.description }}</p>
+                    <a href="">{{ project.url }}</a>
+                </div>
+            </div>
+        </div>
+</template>
+<style lang="scss">
+    
+</style>
