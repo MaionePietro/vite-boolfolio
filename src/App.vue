107ec0d28402a686fc95f712e4716fc94b746d9b
MaionePietro <pietromaionespam@gmail.com>
2023-05-03 16:42:02 +0200

    FIX axsios

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,19 @@
 <script >
-import ProjectCard from './components/ProjectCard.vue'
+
+import project from './components/Project.vue'
 
   export default{
     components: {
-      ProjectCard
+      project
     },
-    data() {
-        return {
-            projects: []
-        }
-    },
-    methods: {
-        fetchProject(){
-            axios.get('http://127.0.0.1:8000/api/project')
-            .then(res => {
-                console.log(res)
-                const { results } = res.data
-                this.projects = results
-            })
-            .catch(err =>{
-                console.log(err)
-            })
-        }
-    },
-    mounted() {
-        this.fetchProject()
-    },
+    
     
   }
 </script>
 
 <template>
-    <div class="container">
-        <div class="row row-cols-4">
-            <div v-for="project in projects">
-                <div class="col">
-                    <div class="card " style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ project.title }}</h5>
-                            <p class="card-text">prova</p>
-                            <p class="card-text"></p>
-                            <p class="card-text"></p>
-                            <a href=""></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="">
+        <project/>
     </div>
 </template>
 
---
+++ src/components/Project.vue
@@ -0,0 +1,43 @@
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            projects: []
+        }
+    },
+    methods: {
+        fetchProject() {
+            axios.get('http://127.0.0.1:8000/api/project')
+                .then(res => {
+                    console.log(res)
+                    const results  = res.data.project
+                    this.projects = results
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    },
+    mounted() {
+        this.fetchProject()
+    },
+}
+</script>
+
+<template lang="">
+    <div class="row row-cols-4">     
+        <div class="col" v-for="(project, i) in projects" :key="project.id">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ project.title }}</h5>
+                    <p class="card-text">{{ project.costumer }}</p>
+                    <p class="card-text">{{ project.description }}</p>
+                    <a href="">{{ project.url }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss"></style>
